b4cafaef6cd7f3185c6a53c250e4ee593f926525
rezwana028 <rezwanarahman028@gmail.com>
2025-07-29 01:34:39 +0600

    task-04 is completed

---
+++ explain.js
@@ -0,0 +1,24 @@
+// ### Task - 4
+//     Explain your answers.
+// What will be the result of the following codes:
+
+// **var a = isNaN(‘11’);**
+
+// **var a = isNaN(2 - 10);**
+
+
+var a = isNaN('11');
+// console.log(a);   //[output is false]
+
+// for this case, the output is false.Because isNaN is used for judge the number which is assigned .If the input assigned part was string then it would show true as the output
+
+var b = isNaN('bristi')
+// console.log(b);       //[ output is true ]
+
+
+var a = isNaN(2 - 10);
+console.log(a);  // [ output is false]
+//  also for this case the same method is working behind 
+
+
+
