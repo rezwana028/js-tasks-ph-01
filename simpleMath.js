8afa76ced11f5afc50492e23431d76a3a66baf94
rezwana028 <rezwanarahman028@gmail.com>
2025-07-29 01:00:49 +0600

    second task completed

---
+++ simpleMath.js
@@ -0,0 +1,13 @@
+// Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+
+var Mathematics = 90;
+var Biology = 95;
+var Chemistry = 89;
+var Physics = 86;
+var Bangla = 90;
+var totalNumber = Mathematics + Biology + Chemistry + Physics + Bangla;
+
+// for calculate the average marks of these subjects we have to devide the totalNumber with the total number of the subjets.
+
+var averageMark = totalNumber / 5;
+console.log(averageMark);
