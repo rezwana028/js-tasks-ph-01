f0e72ce90a2074aafb3efdd8f33f753211c8da99
rezwana028 <rezwanarahman028@gmail.com>
2025-07-29 01:14:12 +0600

    task-03 is completed

---
+++ reminder.js
@@ -0,0 +1,22 @@
+// Your task is to divide the given number by 5 and show the remainder as the output.
+
+// suppose the given number is 119, 246,785,889 & 657
+var number1 = 119;
+var number2 = 246;
+var number3 = 785;
+var number4 = 889;
+var number5 = 657;
+
+// if we want to show the reminder as the output then we have to use % mark 
+
+var remainder1 = number1 % 5;
+var remainder2 = number2 % 5;
+var remainder3 = number3 % 5;
+var remainder4 = number4 % 5;
+var remainder5 = number5 % 5;
+console.log("the reminder number is " + remainder1);
+console.log("the reminder number is " + remainder2);
+console.log("the reminder number is " + remainder3);
+console.log("the reminder number is " + remainder4);
+console.log("the reminder number is " + remainder5);
+
