53a939cd1d10a3319a327b3e13f00ca61fde7171
rezwana028 <rezwanarahman028@gmail.com>
2025-07-29 00:51:32 +0600

    task-01 completed

---
+++ variable.js
@@ -0,0 +1,13 @@
+
+
+// challange no-01
+// You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+var givenMoney = 2000;
+var apple = 300;
+var oranges = 500;
+var grapes = apple * 2;
+var totalCost = apple + oranges + grapes;
+var returnMoney = givenMoney - totalCost;
+
+console.log(returnMoney);
